8dc41b527747ae583105cd0ff367286fcc4c5b76
Ornab95 <ornabbiswass@gmail.com>
2024-11-20 21:01:08 +0600

    First Loop Learning

---
+++ src/CP/LoopLearning.java
@@ -0,0 +1,9 @@
+package CP;
+
+public class LoopLearning {
+    public static void main(String[] args) {
+        for (int x = 0; x < 10; x++) {
+            System.out.println("Ornab Biswass");
+        }
+    }
+}
