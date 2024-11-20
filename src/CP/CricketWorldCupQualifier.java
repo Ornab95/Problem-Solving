f2fc1756e49b900c0ba2b7fd5469c6236fc3361e
Ornab95 <ornabbiswass@gmail.com>
2024-11-20 21:39:37 +0600

    CricketWorldCupQualifier

---
+++ src/CP/CricketWorldCupQualifier.java
@@ -0,0 +1,16 @@
+package CP;
+
+import java.util.Scanner;
+
+public class CricketWorldCupQualifier {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        if (x>=12){
+            System.out.println("Yes");
+        }
+        else System.out.println("No");
+
+    }
+}
